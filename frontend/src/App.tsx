23b79ffd61541a7eb75713314276358390ae128c
abdullahqayid <3boo0odq0v@gmail.com>
2024-02-08 23:50:24 +0300

    initial commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,5 @@
-import { useEffect, useState } from "react";
-import { Note } from "./models/note";
-
 function App() {
-  const [notes, setNotes] = useState<Note[]>([]);
-
-  useEffect(function () {
-    async function getNotes() {
-      try {
-        const res = await fetch("/api/notes", {
-          method: "GET",
-        });
-        const notes = await res.json();
-        console.log(notes);
-        setNotes(notes);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    getNotes();
-  }, []);
-
-  return null;
+  return <div>Hello, World</div>;
 }
 
 export default App;
