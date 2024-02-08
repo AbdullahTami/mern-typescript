2797fc7a5d078eb4930d1d36a9bc4eabb53602f4
abdullahqayid <3boo0odq0v@gmail.com>
2024-02-08 23:53:49 +0300

    proxy setup & server access done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+import { Note } from "./models/note";
+
 function App() {
-  return <div>Hello, World</div>;
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(function () {
+    async function getNotes() {
+      try {
+        const res = await fetch("/api/notes", {
+          method: "GET",
+        });
+        const notes = await res.json();
+        console.log(notes);
+        setNotes(notes);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getNotes();
+  }, []);
+  return null;
 }
 
 export default App;
