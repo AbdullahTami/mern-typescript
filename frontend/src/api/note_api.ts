5368bf979669f863b19764f9a9de38bd70a0f002
abdullahqayid <3boo0odq0v@gmail.com>
2024-02-11 22:01:12 +0300

    client-side api endpoints done

--- frontend/src/api/note_api.ts
+++ frontend/src/api/note_api.ts
@@ -1,4 +1,5 @@
 import { Note } from "../models/note";
+import { User } from "../models/user";
 
 async function fetchData(input: RequestInfo, init?: RequestInit) {
   const res = await fetch(input, init);
@@ -8,6 +9,48 @@ async function fetchData(input: RequestInfo, init?: RequestInit) {
   const errorBody = await res.json();
   const errorMessage = errorBody.error;
   throw Error(errorMessage);
+}
+
+export async function getLoggedInUser(): Promise<User> {
+  const res = await fetchData("api/users", { method: "GET" });
+  return res.json();
+}
+
+export interface SignUpCredentials {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export async function signUp(credentials: SignUpCredentials): Promise<User> {
+  const res = await fetchData("api/users/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
+  return res.json();
+}
+
+export interface LoginCredentials {
+  username: string;
+  password: string;
+}
+
+export async function login(credentials: LoginCredentials): Promise<User> {
+  const res = await fetchData("api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
+  return res.json();
+}
+
+export async function logout() {
+  await fetchData("api/users/logout", { method: "GET" });
 }
 
 export async function fetchNotes(): Promise<Note[]> {
